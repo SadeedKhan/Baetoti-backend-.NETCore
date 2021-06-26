1ceea3842855594a8c898a31da351ce4c71f063f
Sadeed <sadeedullakhan@gmail.com>
2021-06-26 10:15:54 -0700

    Updating

--- Baetoti.Core/Entites/Employee.cs
+++ Baetoti.Core/Entites/Employee.cs
@@ -21,7 +21,6 @@ namespace Baetoti.Core.Entites
         public DateTime? DOB { get; set; }
         public string Phone { get; set; }
         public string Password { get; set; }
-        public int RoleID { get; set; }
         public int ReportTo { get; set; }
         public string Address { get; set; }
         public string Goals { get; set; }
---
+++ Baetoti.Infrastructure/Migrations/20210626164737_updateroleandprivilges.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Baetoti.Infrastructure.Migrations
+{
+    public partial class updateroleandprivilges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoleID",
+                schema: "baetoti",
+                table: "Employee");
+
+            migrationBuilder.CreateTable(
+                name: "Menu",
+                schema: "baetoti",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menu", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Privilege",
+                schema: "baetoti",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Privilege", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RolePrivilege",
+                schema: "baetoti",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleID = table.Column<long>(type: "bigint", nullable: false),
+                    MenuID = table.Column<long>(type: "bigint", nullable: false),
+                    SubMenuID = table.Column<long>(type: "bigint", nullable: false),
+                    PrivilegeID = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: true),
+                    UpdatedBy = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastUpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RolePrivilege", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubMenu",
+                schema: "baetoti",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MenuID = table.Column<long>(type: "bigint", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubMenu", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Menu",
+                schema: "baetoti");
+
+            migrationBuilder.DropTable(
+                name: "Privilege",
+                schema: "baetoti");
+
+            migrationBuilder.DropTable(
+                name: "RolePrivilege",
+                schema: "baetoti");
+
+            migrationBuilder.DropTable(
+                name: "SubMenu",
+                schema: "baetoti");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoleID",
+                schema: "baetoti",
+                table: "Employee",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
