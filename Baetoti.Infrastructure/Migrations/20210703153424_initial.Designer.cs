297a409db75bc14f7fbe366093ae031bfe837d57
Sadeed <sadeedullakhan@gmail.com>
2021-07-03 13:38:07 -0700

    Updating Config and migration

--- Baetoti.Infrastructure/Migrations/20210703153424_initial.Designer.cs
+++ Baetoti.Infrastructure/Migrations/20210703153424_initial.Designer.cs
@@ -4,14 +4,16 @@ using Baetoti.Infrastructure.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Baetoti.Infrastructure.Migrations
 {
     [DbContext(typeof(BaetotiDbContext))]
-    partial class BaetotiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210703153424_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Baetoti.Infrastructure/Migrations/20210703153424_initial.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Baetoti.Infrastructure.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPasswordUpdateRequired",
+                schema: "baetoti",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "LastPasswordChangedDate",
+                schema: "baetoti",
+                table: "User");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsProfileCompleted",
+                schema: "baetoti",
+                table: "User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Picture",
+                schema: "baetoti",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                schema: "baetoti",
+                table: "OTP",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                schema: "baetoti",
+                table: "Item",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                schema: "baetoti",
+                table: "Item",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Reviews",
+                schema: "baetoti",
+                table: "Item",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsProfileCompleted",
+                schema: "baetoti",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                schema: "baetoti",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                schema: "baetoti",
+                table: "OTP");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                schema: "baetoti",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                schema: "baetoti",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "Reviews",
+                schema: "baetoti",
+                table: "Item");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPasswordUpdateRequired",
+                schema: "baetoti",
+                table: "User",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastPasswordChangedDate",
+                schema: "baetoti",
+                table: "User",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
