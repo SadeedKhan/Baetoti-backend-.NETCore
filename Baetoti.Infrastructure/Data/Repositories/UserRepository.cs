c1ea0f285e3a19adf1375f6e846a278eaa0390c2
Muhammad Atif Durrani <matif.durrani@hotmail.com>
2021-07-23 21:13:58 +0500

    Up

--- Baetoti.Infrastructure/Data/Repositories/UserRepository.cs
+++ Baetoti.Infrastructure/Data/Repositories/UserRepository.cs
@@ -9,9 +9,6 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.Entity.Core.Objects;
-using System.Data.Entity.Infrastructure;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
