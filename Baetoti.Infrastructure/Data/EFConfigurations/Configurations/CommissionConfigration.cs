fa83de1dda8ce72f1927282be8161a32ec316776
Sadeed Khan <sadeedullahkhan@gmail.com>
2021-08-23 12:11:53 -0700

    Updating

---
+++ Baetoti.Infrastructure/Data/EFConfigurations/Configurations/CommissionConfigration.cs
@@ -0,0 +1,18 @@
+﻿using Baetoti.Core.Entites;
+using Baetoti.Shared.Enum;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baetoti.Infrastructure.Data.EFConfigurations.Configurations
+{
+    public class CommissionConfigration : IEntityTypeConfiguration<Commissions>
+    {
+        public void Configure(EntityTypeBuilder<Commissions> builder)
+        {
+            builder.ToTable(nameof(Commissions), DBSchema.baetoti.ToString());
+        }
+    }
+}
